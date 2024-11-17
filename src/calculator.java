0edbcfd6dbabe6b693f2be63832b6462970fcc50
Nitish Bhusnoor <nitishbhusnur@gmail.com>
2024-11-17 16:03:46 +0530

    Solved the repeating of results bug.

--- src/calculator.java
+++ src/calculator.java
@@ -1,7 +1,7 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class calculator{
+public class    calculator{
     public static void main(String[] args) {
         var in = new Scanner(System.in);
         var df = new DecimalFormat("#.##");
@@ -35,20 +35,18 @@ public class calculator{
 
         for (int n = 0; n < no_of_course; n++){
             if ( no_of_classes[n] <= total_class[n]){
-                for(int k = 0; k < no_of_course; k++){
-                    percentage_of_course[k] = (float) no_of_classes[k] /total_class[k];
-                    percentage_of_course[k] *= 100;
-                    System.out.println(course[k]+": "+df.format(percentage_of_course[k])+" %");
-                    should_attend_or_not[k] = (percentage_criteria * total_class[k] - 100 * no_of_classes[k])/(100 - percentage_criteria);
-                }
-                for(int l = 0; l < no_of_course; l++){
-                    if(should_attend_or_not[l] > 0){
-                        System.out.println("You should attend next " + should_attend_or_not[l]+ " classes of "+ course[l] +" to get on back on track");
+                    percentage_of_course[n] = (float) no_of_classes[n] /total_class[n];
+                    percentage_of_course[n] *= 100;
+                    System.out.println(course[n]+": "+df.format(percentage_of_course[n])+" %");
+                    should_attend_or_not[n] = (percentage_criteria * total_class[n] - 100 * no_of_classes[n])/(100 - percentage_criteria);
+
+                    if(should_attend_or_not[n] > 0){
+                        System.out.println("You should attend next " + should_attend_or_not[n]+ " classes of "+ course[n] +" to get on back on track");
                     }
                     else {
-                        System.out.println("You can bunk next " + ((100 * no_of_classes[l] - percentage_criteria * total_class[l])/percentage_criteria) + " classes of "+ course[l] );
+                        System.out.println("You can bunk next " + ((100 * no_of_classes[n] - percentage_criteria * total_class[n])/percentage_criteria) + " classes of "+ course[n] );
                     }
-                }
+
             }
             else{
                 throw new IllegalArgumentException(" No.of Initial classes can not be greater than Total No. of classes");
